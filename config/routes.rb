23e842e652c2eced8fa54701c0fe40078863ea86
fitttdev <nima.yonten1729@gmail.com>
2024-02-08 21:59:01 +0600

    generate devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  scope 'api/v1' do
+    devise_for(
+      :users,
+      controllers: {
+        invitations: 'api/v1/invitations',
+        sessions: 'api/v1/sessions',
+        registrations: 'api/v1/registrations',
+        passwords: 'api/v1/passwords'
+      },
+      defaults: { format: :json },
+      path: '',
+      path_names: { password: 'password' }
+    )
+  end
 end
