2e75705b44d0dacf51de976680489b25eaf38775
fitttdev <nima.yonten1729@gmail.com>
2024-02-09 17:02:47 +0600

    add log responder

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,5 +12,25 @@ module Users
         data: resource
       }
     end
+
+    def respond_to_on_destroy
+      jwt_payload = JWT.decode(
+        request.headers['Authorization'].split[1],
+        ENV.fetch('DEVISE_JWT_SECRET_KEY', nil)
+      ).first
+      current_user = User.find(jwt_payload['sub'])
+
+      if current_user
+        render json: {
+          status: 200,
+          message: 'Logged out successfully'
+        }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: 'User has no active session'
+        }, status: :unauthorized
+      end
+    end
   end
 end
