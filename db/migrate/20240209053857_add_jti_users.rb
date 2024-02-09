099606d91dbb080079ae99943044516e6e93e143
fitttdev <nima.yonten1729@gmail.com>
2024-02-09 13:41:06 +0600

    add jti migration

--- db/migrate/20240209053857_add_jti_users.rb
+++ db/migrate/20240209053857_add_jti_users.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class AddJtiUsers < ActiveRecord::Migration[7.1]
-  def change; end
+  def change
+    add_column :users, :jti, :string, default: '', null: false
+    add_index :users, :jti, unique: true
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_09_053857) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "jti", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
