e15b74d9d57ac2370a65dea35cc4d64163063777
fitttdev <nima.yonten1729@gmail.com>
2024-02-09 13:33:31 +0600

    rename to hako

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "relic_production"
+  # config.active_job.queue_name_prefix = "hako_production"
 
   config.action_mailer.perform_caching = false
 
