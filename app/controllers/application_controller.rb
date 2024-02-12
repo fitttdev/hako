76800a4050c388b22e0461b504f44d08968a426f
fitttdev <nima.yonten1729@gmail.com>
2024-02-12 22:21:53 +0600

    add user assigner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include CurrentUserAssigner
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    @current_user = assign_current_user
+  end
 end
